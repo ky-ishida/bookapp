1658ccf3142cb78a5af458a6b51fa04e0af4cd08
ky-ishida <k.ishida.official@gmail.com>
2023-10-08 00:49:39 +0900

    feat:一覧取得

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -3,8 +3,9 @@
     {{-- Bootstrapの定型コード --}}
     <div class="card-body">
         <div class="card-body">
-            本のタイトル
+            本詳細登録
         </div>
+
         @include('common.errors')
 
         {{-- 本登録フォーム --}}
@@ -12,9 +13,25 @@
             @csrf
 
             {{-- 本のタイトル --}}
-            <div class="form-group">
-                <div class="col-sm-6">
-                    <input type="text" name="input_name" class="form-control">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">名前</label>
+                    <input type="text" name="item_name" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="omount" class="col-sm-3 control-label">金額</label>
+                    <input type="text" name="item_amount" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="number" class="col-sm-3 control-label">数</label>
+                    <input type="date" name="item_number" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="published" class="col-sm-3 control-label">公開日</label>
+                    <input type="text" name="published" class="form-control">
                 </div>
             </div>
 
@@ -26,6 +43,55 @@
                     </button>
                 </div>
             </div>
-        </form>
+    
+    </form>
     </div>
+
+    {{-- 登録している本の表示 --}}
+    @if (count($books) > 0)
+        <div class="card-body">
+            <div class="card-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        {{-- テーブルヘッダー --}}
+                        <th>本一覧</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    {{-- テーブル本体 --}}
+                    <tbody>
+                        @foreach ($books as $book)
+                            <tr>
+                                {{-- 本のタイトル     --}}
+                                <td class="table-text">
+                                    <div>{{ $book->item_name }}</div>
+                                </td>
+                                {{-- 本更新ボタン --}}
+                                <td>
+                                    <form action="{{ url('update/' . $book->id) }}" method="POST">
+                                        {{-- CSRFからの保護 --}}
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">
+                                            更新
+                                        </button>
+                                    </form>
+                                </td>
+                                <td>
+                                    {{-- 本の削除ボタン --}}
+                                    <form action="{{ url('book/' . $book->id) }}"method="POST">
+                                        {{-- CSRFからの保護 --}}
+                                        @csrf
+                                        {{-- 疑似フォームメソッド --}}
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">
+                                            削除
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,56 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 登録してあるデーター一覧を取得する
 Route::get('/', function () {
-    return view('books');
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', ['books' => $books]);
 });
+// 本の追加
 Route::post('/books', function (Request $request) {
+    // 本の追加
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max255',
+        'item_number' => 'required|min1|max3',
+        'item_amount' => 'required|max6',
+        'published' => 'required'
+
+    ]);
+
+    // バリデーションエラー
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
+    // 削除したあとはbookページにリダイレクト
+    return  redirect('/');
 });
 
+// データーの削除
 Route::delete('/book/{book}', function (Book $book) {
+    $book->delete();
+    // 削除した後bookページにリダイレクト
+    return redirect('/');
 });
+
+// データの更新する
+Route::post(
+    '/update/{books}',
+    function (Book $books) {
+        // {books}id値を取得→BOOK ＄book　id値の１レコードを取得する
+        return view('update', ['book' => $books]);
+    }
+);
+
+// ログイン認証機能を呼び出すコマンド
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
